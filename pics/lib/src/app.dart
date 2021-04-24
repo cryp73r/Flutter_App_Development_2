c38b7da3604a5c851e399300290fdfef86b94c9f
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-04-24 23:31:55 +0530

    First Application

---
+++ pics/lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => debugPrint("Action Button Pressed"),
+        ),
+        appBar: AppBar(
+          title: Text("Let's see Images"),
+        ),
+      ),
+    );
+  }
+}
