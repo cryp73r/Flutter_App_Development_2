0bdf337cc165b33df295ce0485a75080e4e4d743
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-04-26 21:17:53 +0530

    Working on APIs

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,18 +1,40 @@
+import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 
-// StatelessWidget Starting.
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response = await get(Uri.parse("https://jsonplaceholder.typicode.com/photos/$counter"));
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () => debugPrint("Action Button Pressed"),
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: Text("Let's see Images"),
         ),
+        body: ImageList(images),
       ),
     );
   }
