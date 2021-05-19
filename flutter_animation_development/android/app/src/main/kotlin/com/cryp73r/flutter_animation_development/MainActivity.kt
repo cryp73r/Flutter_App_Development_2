631ae04e7314c320b97be945ab1f3001a7aed9b9
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-05-19 23:06:41 +0530

    Flutter Animation

---
+++ flutter_animation_development/android/app/src/main/kotlin/com/cryp73r/flutter_animation_development/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cryp73r.flutter_animation_development
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_animation_development/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animation_development/src/app.dart';
+
+void main() {
+  runApp(App());
+}
